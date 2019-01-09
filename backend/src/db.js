edd69ecc72deccdea7ea66c1855838940392cef6
Sam <sammintz1993@gmail.com>
2019-01-09 11:21:19 -0600

    Set up db.js on backend to connect to prisma db

---
+++ backend/src/db.js
@@ -0,0 +1,7 @@
+// connect to prisma
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+});
