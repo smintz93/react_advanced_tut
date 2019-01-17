230372737999ea540434d48f09fab76560962cdc
Sam <sammintz1993@gmail.com>
2019-01-17 11:32:23 -0600

    getInititalProps next.js method to crawl pages before rendering

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,12 @@
+const { forwardTo } = require('prisma-binding');
+// forwarding yoga to graphql 
+
+
 const Query = {
-  // dogs(parent, args, ctx, info) {
-  //   return [{ name: 'Snickers' }, { name: 'Sunny' }];
+  items: forwardTo('db')
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
   // }
 };
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,33 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  // next.js life cycle method. will run before render.
+  // going to crawl pages and see what needs to be feteched before render method runs
+  static async getInitialProps({ Component, ctx}) {
+    let pageProps = {};
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user 
+     pageProps.query = ctx.query;
+     return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo } = this.props;
     // Component in this case is whatever component you are trying to render 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
