0acd23e48ff66dcc7d94159393e523ae965ae0b5
Sam <sammintz1993@gmail.com>
2019-01-17 12:18:16 -0600

    Display price, description, and buttons

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
 
 class Item extends Component {
   static propTypes = {
@@ -15,7 +17,31 @@ class Item extends Component {
     return (
       <div>
         <ItemStyles>
-          <Title>{item.title}</Title>
+          
+          {item.image ? <img src={item.image} alt={item.title} /> : null}
+
+          <Title>
+            <Link href={{
+              pathname: './item',
+              query: { id: item.id }
+            }}>
+              <a>{item.title}</a>
+            </Link>
+          </Title>
+          <PriceTag>
+            {formatMoney(item.price)}
+          </PriceTag>
+          <p>{item.description}</p>
+          <div className="buttonList">
+            <Link href={{
+              pathname: "update",
+              query: { id: item.id }
+            }}>
+              <a>Edit </a>
+            </Link>
+            <button>Add to Card</button>
+            <button>Delete</button>
+          </div>
         </ItemStyles>
       </div>
     );
