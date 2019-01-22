eefaa6c49b9c961b408918e9df313af8eb4dfa2c
Sam <sammintz1993@gmail.com>
2019-01-22 11:11:26 -0600

    use computed property names to set state in handleChange method

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -5,9 +5,50 @@ import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney'
 
 class CreateItem extends Component {
+  state = {
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0
+  };
+
+  handleChange = (e) => {
+    const { name, type, value } = e.target;
+    const val = type === 'number' ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
   render() {
     return (
       <Form>
+        <fieldset>
+          <label htmlFor="title">
+            Title
+                <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="price">
+            Price
+                <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+        </fieldset>
         <h2>Sell an Item.</h2>
       </Form>
     );
