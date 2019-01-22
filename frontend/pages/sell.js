dbf17abd7d3de48b22330e93f24dbcc150b25df5
Sam <sammintz1993@gmail.com>
2019-01-22 10:42:48 -0600

    CreateItem form rendering at sell page

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,10 @@
 // next.js takes care of import react in this case. 
 import Link from 'next/link';
+import CreateItem from '../components/CreateItem';
 
 const Sell = (props) => (
   <div>
-    <p>Sell!</p>
+    <CreateItem />
   </div>
 );
 
