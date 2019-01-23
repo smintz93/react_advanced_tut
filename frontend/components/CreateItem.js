b3d0fc415fe28f5087601d161172c06d2a01cfbb
Sam <sammintz1993@gmail.com>
2019-01-23 12:33:06 -0600

    Upload images using cloudinary

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -42,6 +42,27 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async (e) => {
+    console.log('Uploading File...')
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const response = await fetch('https://api.cloudinary.com/v1_1/dihkh6c79/image/upload',
+      {
+        method: "POST",
+        body: data
+      });
+
+    const file = await response.json();
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -62,6 +83,17 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                    <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an Image"
+                  required
+                  onChange={this.uploadFile}
+                />
+              </label>
               <label htmlFor="title">
                 Title
                     <input
